cd0fa8dd60e771cf1b919e8753166f89b87c32cc
Muhammed Kadir KARAKOC <m.kadirkarakoc@gmail.com>
2020-12-06 01:09:03 +0300

    Install Jenkins on EC2 and ssh connection

--- main.tf
+++ main.tf
@@ -56,6 +56,30 @@ resource "aws_instance" "jenkins" {
   instance_type = "t2.micro"
   security_groups = [aws_security_group.web_traffic.name]
   key_name = "project1"
+
+  provisioner "remote-exec" {
+    inline = [
+      "wget -q -O - https://pkg.jenkins.io/debian-stable/jenkins.io.key | sudo apt-key add -",
+      "sudo sh -c 'echo deb http://pkg.jenkins.io/debian-stable binary/ > /etc/apt/sources.list.d/jenkins.list'",
+      "sudo apt update -qq",
+      "sudo apt install -y default-jre",
+      "sudo apt install -y jenkins",
+      "sudo systemctl start jenkins",
+      "sudo iptables -t nat -A PREROUTING -p tcp --dport 80 -j REDIRECT --to-port 8080",
+      "sudo sh -c \"iptables-save > /etc/iptables.rules\"",
+      "echo iptables-persistent iptables-persistent/autosave_v4 boolean true | sudo debconf-set-selections",
+      "echo iptables-persistent iptables-persistent/autosave_v6 boolean true | sudo debconf-set-selections",
+      "sudo apt-get -y install iptables-persistent",
+      "sudo ufw allow 8080",
+    ]
+  }
+
+  connection {
+    type        = "ssh"
+    host        = self.public_ip
+    user        = "ubuntu"
+    private_key = file("~/project1.pem")
+  }
   
   tags = {
     "Name"      = "Jenkins_Server"
